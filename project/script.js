e475bdef9c73ec6945976ebf3cfe40bee627c581
HieuMai2612 <hieuduke@gmail.com>
2024-06-09 08:53:44 +0700

    123

---
+++ project/script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+};
+
+function momodal(){
+  document.getElementById("nenmodal-1").classList.toggle("active");
+  }
+  function momodal1(){
+    document.getElementById("nenmodal-2").classList.toggle("active");
+    }
+  
